90e43f12ac8f0f39647f05a55ed009c6d7e3ec54
Andrew Sirenko <Andrew@andrews-mbp-3.attlocal.net>
2020-07-30 03:13:14 -0700

    Copied file auto added to weekly reports folder

--- googleDocs.js
+++ googleDocs.js
@@ -1,5 +1,8 @@
 const auth = require('./auth.js');
 
+// FolderId of folder which stores weekly reports
+const folderId = '1z8V9bFSBL94TlG-qY6yb_DUHqbwycSwK';
+
 async function generateWeeklyReportDoc() {
     //Authorization from auth.js
     const docs = await auth.authorizeDocs();
@@ -20,6 +23,7 @@ async function generateWeeklyReportDoc() {
 }
 
 // Copies letter template document using Drive API then
+//   adds copy to Weekly Reports folder: https://drive.google.com/drive/folders/1z8V9bFSBL94TlG-qY6yb_DUHqbwycSwK?usp=sharing
 //   returns file ID of (new) copy.
 async function copyFile(originFileId, copyTitle) {
     const drive = await auth.authorizeDrive();
@@ -28,10 +32,11 @@ async function copyFile(originFileId, copyTitle) {
         fileId: originFileId,
         requestBody: {
             name: copyTitle,
+            addParents: folderId,
         },
     });
 
     console.log(request.data);
 }
 
-copyFile('13z0nQfjAa-DzD4Rq1WDOxoOTZ4GXXjB2oYWWQsPgPvY', 'test');
+copyFile('13z0nQfjAa-DzD4Rq1WDOxoOTZ4GXXjB2oYWWQsPgPvY', 'test3');
