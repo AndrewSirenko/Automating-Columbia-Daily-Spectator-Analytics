852fa87574ccf7f086de58a513cc1b30d4a2b058
Andrew Sirenko <Andrew@andrews-mbp-3.attlocal.net>
2020-07-29 22:49:29 -0700

    generateWeeklyReportData works for all sections

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const processData = require('./processData');
 // GETs data from Analytics API and POSTs to Google Doc template
 async function generateWeeklyReport() {
     console.log('\nGenerating Weekly Report...\n');
+    // Generates, processes, cleans, and compiles CDS weekly report data
     processData.generateWeeklyReportData();
 }
 
